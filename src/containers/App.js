287a5a4a668df601803ab8ced6b7f15d7447ba59
rcioara <roxy@Roxanas-MacBook-Pro.local>
2018-06-06 11:23:07 +0300

    test

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,67 +1,67 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classes from './../components/Cockpit/Cockpit.css';
-import Persons from './../components/Persons/Persons'
-import Cockpit from './../components/Cockpit/Cockpit'
+import Persons from './../components/Persons/Persons';
+import Cockpit from './../components/Cockpit/Cockpit';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    console.log('[App.js] - Inside constructor() method');
+  constructor (props) {
+    super (props);
+    console.log ('[App.js ] - Inside constructor() method');
   }
 
-  componentWillMount() {
-    console.log('[App.js] - Inside componentWillMount() method');
+  componentWillMount () {
+    console.log ('[App.js] - Inside componentWillMount() method');
   }
 
-  componentDidMount() {
-    console.log('[App.js] - Inside componentDidMount() method');
+  componentDidMount () {
+    console.log ('[App.js] - Inside componentDidMount() method');
   }
 
   state = {
     persons: [
-      { id: 1, name: 'Max', age: '30', username: 'max' },
-      { id: 2, name: 'Roxi', age: '28', username: 'roxi' },
-      { id: 2, name: 'Oana', age: '29', username: 'oana' }
+      {id: 1, name: 'Max', age: '30', username: 'max'},
+      {id: 2, name: 'Roxi', age: '28', username: 'roxi'},
+      {id: 2, name: 'Oana', age: '29', username: 'oana'},
     ],
-    showPersons: false
+    showPersons: false,
   };
 
   togglePersonsHandler = () => {
-    this.setState({ showPersons: !this.state.showPersons });
-  }
+    this.setState ({showPersons: !this.state.showPersons});
+  };
 
-  deletePersonHandler = (idx) => {
+  deletePersonHandler = idx => {
     const persons = [...this.state.persons];
-    persons.splice(idx, 1);
-    this.setState({ persons: persons });
-  }
+    persons.splice (idx, 1);
+    this.setState ({persons: persons});
+  };
 
   changeNameHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => id === p.id);
-    const person = { ...this.state.persons[personIndex] };
+    const personIndex = this.state.persons.findIndex (p => id === p.id);
+    const person = {...this.state.persons[personIndex]};
 
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState ({persons: persons});
+  };
 
-  }
-
-  render() {
-    console.log('[App.js] - Inside render() method');
+  render () {
+    console.log ('[App.js] - Inside render() method');
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = (<div>
-        <Persons
-          persons={this.state.persons}
-          clicked={this.deletePersonHandler}
-          changed={this.changeNameHandler}>
-        </Persons>
-      </div>);
+      persons = (
+        <div>
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.changeNameHandler}
+          />
+        </div>
+      );
     }
 
     return (
@@ -69,8 +69,8 @@ class App extends Component {
         <Cockpit
           persons={this.state.persons}
           showPersons={this.state.showPersons}
-          clicked={this.togglePersonsHandler}>
-        </Cockpit>
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
